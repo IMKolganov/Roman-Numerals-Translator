07473e65a4e31542e873f91a65726a330001da5c
Иван Колганов <110064602+IMKolganov@users.noreply.github.com>
2023-09-13 04:42:37 +0300

    add task in comment

--- Roman-Numerals-Translator/Program.cs
+++ Roman-Numerals-Translator/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Create an implementation of a function that converts an integer input into a string, 
+containing a valid roman number.
+List of all possible roman literals: 
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+
+I may go before V and X (10)
+X may go before L (50) and C
+C may go before D (500) and M
+
+Table with all possible digits in roman system:
+            1 2  3   4  5 6  7   8    9
+1-9         I II III IV V VI VII VIII IX
+10-90       X XX XXX XL L LX LXX LXXX XC
+100-900     C CC CCC CD D DC DCC DCCC CM
+1000-3000   M MM MMM
+
+Examples:
+11 => X I
+123 => C XX III
+498 => CD XC VIII
+1987 => M CM LXXX VII
+2023 => MM XX III
+
+1645 => M DC XL V
+504 => 
+
+*/
 
